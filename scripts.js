bfa6f155e471f14948a3a2445de893c96de75597
LeonardoTironi <leotironineto@gmail.com>
2024-11-17 11:37:08 -0300

    Final

--- scripts.js
+++ scripts.js
@@ -31,8 +31,13 @@ function ex2(){
     let num2 = parseInt(ponto2.value);
     let num3 = parseInt(ponto3.value);
     let num4 = parseInt(ponto4.value);
-    let resultado = (num1-num3)*(num1-num3)+(num2-num4)*(num2-num4);
-    alert(`Resultado: ${resultado}`);
+    if(!isNaN(num1) && !isNaN(num2) && !isNaN(num3) && !isNaN(num4)){
+        let resultado = (num1-num3)*(num1-num3)+(num2-num4)*(num2-num4);
+        alert(`Resultado: ${resultado}`);
+    }
+    else{
+        alert("Digite apenas números");
+    }
 }
 
 function ex3(){
@@ -58,9 +63,29 @@ function ex4(){
     let total = texto.split(" ");
     console.log(linhas.length);
     console.log(total.length);
-    let colunas = total.length/linhas.length
-    alert(`Essa é uma matriz de dimensão ${linhas.length}x${colunas}\nA matriz tem ${linhas.length} linhas e ${colunas} colunas.`)
-
-
+    let colunas = total.length/linhas.length;
+    alert(`Essa é uma matriz de dimensão ${linhas.length}x${colunas}\nA matriz tem ${linhas.length} linhas e ${colunas} colunas.`);
     //linhas[linhas.length-1] para testar se o último elemento é ";", mas não é necessário para o exercício eu acho
+}
+
+function ex5(){
+    const element = document.getElementById("fibonacci");
+    let num = parseInt(element.value);
+    let lista=[];
+    let resultado = 0;
+    let soma = 1;
+    let temp = 0;
+    if(!isNaN(num)){
+        while (num>0){
+            num--;
+            lista.push(resultado);
+            temp = soma+resultado
+            resultado=soma;
+            soma=temp;
+        }
+        alert(lista);
+    }
+    else{
+        alert("Digite um número");
+    }
 }
